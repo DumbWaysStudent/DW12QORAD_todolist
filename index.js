f212f97bf56019326ae5a17992408651ca5deca9
nurmuhamadrum <nurmuhamadrum@gmail.com>
2019-10-07 08:23:34 +0700

    add 3.delete_todo

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Todo from './src/Todo';
 import Addtodo from './src/Addtodo';
+import DeleteTodo from './src/DeleteTodo';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Addtodo);
+AppRegistry.registerComponent(appName, () => DeleteTodo);
